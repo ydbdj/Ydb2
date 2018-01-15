44bb5ac05746f899f5a35f29455313fb113be506
ydbdj <m18701231935@163.com>
2018-01-15 18:24:32 +0800

    init

--- app/src/main/java/com/example/hp/ydb2/HomeActivity.java
+++ app/src/main/java/com/example/hp/ydb2/HomeActivity.java
@@ -3,16 +3,11 @@ package com.example.hp.ydb2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initView();
-    }
-
-    private void initView() {
-
+        setContentView(R.layout.activity_home);
     }
 }
